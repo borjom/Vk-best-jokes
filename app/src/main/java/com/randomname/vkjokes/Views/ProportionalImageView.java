05fc14218b07ee18a2cc0d2249bb0096d9833bf0
Vlad <slayerru@yandex.ru>
2015-10-23 00:23:34 +0300

    fixed multiple images layout

--- app/src/main/java/com/randomname/vkjokes/Views/ProportionalImageView.java
+++ app/src/main/java/com/randomname/vkjokes/Views/ProportionalImageView.java
@@ -6,27 +6,51 @@ import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class ProportionalImageView extends ImageView {
+    public ProportionalImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
     public ProportionalImageView(Context context) {
         super(context);
     }
 
-    public ProportionalImageView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public ProportionalImageView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         Drawable d = getDrawable();
-        if (d != null) {
-            int w = MeasureSpec.getSize(widthMeasureSpec);
-            int h = w * d.getIntrinsicHeight() / d.getIntrinsicWidth();
-            setMeasuredDimension(w, h);
+        if (d == null) {
+            super.setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+            return;
         }
-        else super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int imageHeight = d.getIntrinsicHeight();
+        int imageWidth = d.getIntrinsicWidth();
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        float imageRatio = 0.0F;
+        if (imageHeight > 0) {
+            imageRatio = imageWidth / imageHeight;
+        }
+        float sizeRatio = 0.0F;
+        if (heightSize > 0) {
+            sizeRatio = widthSize / heightSize;
+        }
+
+        int width;
+        int height;
+        if (imageRatio >= sizeRatio) {
+            // set width to maximum allowed
+            width = widthSize;
+            // scale height
+            height = width * imageHeight / imageWidth;
+        } else {
+            // set height to maximum allowed
+            height = heightSize;
+            // scale width
+            width = height * imageWidth / imageHeight;
+        }
+
+        setMeasuredDimension(width, height);
     }
 }
